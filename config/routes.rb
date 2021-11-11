1a94129b2b06bd80de7fbf1d96588827a99be9c7
saqib010 <010saqib@gmail.com>
2021-11-11 18:36:18 +0500

    devise routes changed for login, logout, register

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
  
-  devise_for :users
+  devise_for :users, path: '', path_names: {sign_in: 'login', sign_out: 'logout', sign_up: 'register'}
   #get 'pages/home'
   #get 'pages/about'
   get 'angular-items', to: 'portfolios#angular'
